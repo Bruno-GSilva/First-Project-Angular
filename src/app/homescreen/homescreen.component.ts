57f69708051472d69f9dfe46441fab31b2c23f3f
BRUNO GOMES DA SILVA <BRUNO6162@DIGITALCOLLEGE.LOCAL>
2023-11-30 15:42:22 -0300

    Initial commit

---
+++ src/app/homescreen/homescreen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homescreen',
+  standalone: true,
+  imports: [],
+  templateUrl: './homescreen.component.html',
+  styleUrl: './homescreen.component.css'
+})
+export class HomescreenComponent {
+  title = 'homescreen'
+}
